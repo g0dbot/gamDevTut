9c34e6e1addb954581d37ccca2cc70a146402c49
Ronel Ramtahal <ronelkieferramtahal@gmail.com>
2024-12-27 20:50:26 -0400

    showing enemy animation

--- 3enemyMvmntPatterns/enemyMvmntPatterns.js
+++ 3enemyMvmntPatterns/enemyMvmntPatterns.js
@@ -6,14 +6,16 @@ const CANVAS_WIDTH = canvas.width = 500;
 const CANVAS_HEIGHT = canvas.height = 1000;
 
 enemy1 = {
-    x: 0,
-    y: 0,
+    x: 10,
+    y: 50,
     width: 200,
     height: 200
 }
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    enemy1.y++;
+    enemy1.x++;
     ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
     requestAnimationFrame(animate);
 }

fb864a317186f65c53e59d08ecc1339564016ed0
Ronel Ramtahal <ronelkieferramtahal@gmail.com>
2024-12-27 20:05:44 -0400

    added load function to ensure assets load before js run

--- 2parallaxBg/parallaxBg.js
+++ 2parallaxBg/parallaxBg.js
@@ -29,18 +29,6 @@ backgroundLayer4.src = "../res/bg/layer-4.png";
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = "../res/bg/layer-5.png";
 
-//sldier game speed
-const slider = document.getElementById("slider");//vcariable to store and ret slider values
-slider.value = gameSpeed;//init page load set var value to gamespeed settings
-const showGameSpeed = document.getElementById("showGameSpeed");//variable to store and ret slider values
-showGameSpeed.innerHTML = gameSpeed;//init page load set var value to gamespeed settings
-
-//to change scrolling speed based on user input
-slider.addEventListener("change", function(e){
-    gameSpeed = e.target.value;
-    showGameSpeed.innerHTML = e.target.value;
-});
-
 //class to hold each background layer
 class Layer {
     constructor(image, speedModifier){
@@ -73,29 +61,50 @@ class Layer {
     }
 }
 
+//online game requires html and assets load before js can run
+window.addEventListener("load", function(){
+    //sldier game speed
+    const slider = document.getElementById("slider");//vcariable to store and ret slider values
+    slider.value = gameSpeed;//init page load set var value to gamespeed settings
+    const showGameSpeed = document.getElementById("showGameSpeed");//variable to store and ret slider values
+    showGameSpeed.innerHTML = gameSpeed;//init page load set var value to gamespeed settings
 
-//instantiate each layer object
-const layer1 = new Layer(backgroundLayer1, 0.2);
-const layer2 = new Layer(backgroundLayer2, 0.4);
-const layer3 = new Layer(backgroundLayer3, 0.6);
-const layer4 = new Layer(backgroundLayer4, 0.8);
-const layer5 = new Layer(backgroundLayer5, 1);
-
-//declare array to store layers
-const layerObjects = [layer1, layer2, layer3, layer4, layer5];
-
-//drawing function to animate background
-function animate(){
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);//clears canvas
-    
-    //updae and draw each layer
-    layerObjects.forEach(layer => {
-        layer.update();
-        layer.draw();
+    //to change scrolling speed based on user input
+    slider.addEventListener("change", function(e){
+        gameSpeed = e.target.value;
+        showGameSpeed.innerHTML = e.target.value;
     });
 
-    requestAnimationFrame(animate);//calls function over and over again
-};
+    //instantiate each layer object
+    const layer1 = new Layer(backgroundLayer1, 0.2);
+    const layer2 = new Layer(backgroundLayer2, 0.4);
+    const layer3 = new Layer(backgroundLayer3, 0.6);
+    const layer4 = new Layer(backgroundLayer4, 0.8);
+    const layer5 = new Layer(backgroundLayer5, 1);
 
-//call function to animate
-animate();
+    //declare array to store layers
+    const layerObjects = [layer1, layer2, layer3, layer4, layer5];
+
+    //drawing function to animate background
+    function animate(){
+        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);//clears canvas
+        
+        //updae and draw each layer
+        layerObjects.forEach(layer => {
+            layer.update();
+            layer.draw();
+        });
+
+        requestAnimationFrame(animate);//calls function over and over again
+    };
+    
+    //call function to animate
+    animate();
+});
+
+
+
+
+
+
+

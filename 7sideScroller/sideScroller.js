b3355e54280736e4f5151aa9f7654e5bf4b7b5c4
Ronel Ramtahal <ronelkieferramtahal@gmail.com>
2025-01-07 23:13:41 -0400

    Proj 7 Setup

---
+++ 7sideScroller/sideScroller.js
@@ -0,0 +1,6 @@
+//load event waits for all asses to be fully loaded before
+//running code in anonymous callback function
+//anon func means func without name
+window.addEventListener("load", function() {
+    
+})
